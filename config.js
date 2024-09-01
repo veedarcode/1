4e49f30e65c3d7163a41b5bf90a89ce6bec660b2
veedar-char <155560374+veedarcode@users.noreply.github.com>
2024-09-02 00:04:05 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Batin",
+    introTitle: 'Renaissance',
+    introDesc: `Bukankah kamu yang telah meninggalkan seseorang,
+    dan sekarang merindukan yang kau tinggalkan demi yang kau kejar itu?
+    sekarang telah kau sadar, untuk merindukan masa lalumu.
+    entah perihal tentang apa yang kau sadari itu`,
+    btnIntro: 'sudahkah rindu?',
+    title: 'Kalau begitu, apakah kamu bersedia kembali ke rumah lama-mu?',
+    desc: 'Mungkin tali merah memisahkan kita tetapi akhirnya kita tetap terikat',
+    btnYes: 'Iya, kamu salah tetapi aku mau kita kembali seperti dulu!!!',
+    btnNo: 'Tidak, kamu jahat karena meninggalkanku, telah kutunggu kamu mengapa kita malah asing!!cukup aku close web ini.',
+    question: 'Maafkanku yang meninggalkanmu sendiri, aku mengerti aku jahat meninggalkan seorang Luna merangkul hidupnya sendiri. Tetapi, ini adalah respon terakhirku akan kutanyakan "apakah kamu bersedia kembali ke-rangkulanku?" ',
+    btnReply: '.....',
+    reply: 'iya, aku kangenn kenapa malah asing! ayo chat aku lagiii!',
+    mess: 'aku sudah tauu',
+    messDesc: 'ayo bersama lagiii, chat dan kembali (dm mu diprivat anyway)',
+    btnAccept: 'Okiiiii lun',
+    messLink: 'https://www.instagram.com/reel/C5WS-MDP3H4/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' 
+}
